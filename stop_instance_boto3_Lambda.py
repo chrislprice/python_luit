24b6b9df58ec26c5ce5f7c454e3011a60c288ff5
chrislprice <christopherlprice@gmail.com>
2023-02-15 19:48:19 +0000

    had new stop command

--- stop_instance_boto3_Lambda.py
+++ stop_instance_boto3_Lambda.py
@@ -1,15 +1,15 @@
 import boto3
-
 ec2 = boto3.resource('ec2')
 
 def lambda_handler(event,context):
-    instances2 = ec2.instances
-    instances = ec2.instances.filter(Filters=[{'Name:': 'Environment','Values': ['Dev']},{'Name': 'instance-state-name', 'Values': ['running']}])
+    instances = ec2.instances.filter(Filters=[{'Name:': 'tag:Environment','Values': ['Dev']},{'Name': 'instance-state-name', 'Values': ['running']}])
+    #print(instances)
 
-    print(instances2)
     for instance in instances:
         instance_id = instance.id
-        print("Stopping Instance: ", instance_id)
-        ec2.instances('instance_id').stop()
+       # print("Stopping Instance:", instance.id)
+        ec2.instances.filter(InstanceIds=instance_id).stop()
+        #instance.stop()
+        #ec2.instances(instance_id).stop()
     
     return "completed!"
